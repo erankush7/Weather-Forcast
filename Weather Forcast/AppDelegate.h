be3db2be89532f8612d4314eab0f5396e76e41b0
erankush7 <erankush7@gmail.com>
2014-04-28 23:23:23 +0530

    Initial Commit

---
+++ Weather Forcast/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weather Forcast
+//
+//  Created by Ankush Sharma on 28/04/14.
+//  Copyright (c) 2014 Ankush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Weather Forcast/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Weather Forcast
+//
+//  Created by Ankush Sharma on 28/04/14.
+//  Copyright (c) 2014 Ankush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
